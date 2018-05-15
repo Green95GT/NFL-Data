f65ccaaea42e869f4a24d8fa23518e2cbbfc2fb6
Green95GT <39316361+Green95GT@users.noreply.github.com>
2018-05-15 17:52:09 -0400

    Create ColumnData.py

---
+++ ColumnData.py
@@ -0,0 +1,22 @@
+class Dataset:
+    def __init__(self, data):
+        self.header = data[0]
+        self.data = data[1:]
+    
+    def column(self, label):
+        if label not in self.header:
+            return None
+        
+        index = 0
+        for idx, element in enumerate(self.header):
+            if label == element:
+                index = idx
+        
+        column = []
+        for row in self.data:
+            column.append(row[index])
+        return column
+
+nfl_dataset = Dataset(nfl_data)
+year_column = nfl_dataset.column('year')
+player_column = nfl_dataset.column('player')
