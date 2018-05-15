1fefea5350eebd297ccb63d7b4670b86aa1c46c2
Green95GT <39316361+Green95GT@users.noreply.github.com>
2018-05-15 18:00:34 -0400

    Create Num_of_Wins_Fixed.py

---
+++ Num_of_Wins_Fixed.py
@@ -0,0 +1,10 @@
+import csv
+f = open("nfl.csv", "r")
+nfl = list(csv.reader(f))
+patriots_wins = 0
+
+# Loop through the list of rows, each time "New England Patriots" is found
+# in the third column (index value of 2), increase `patriots_wins` by 1
+for row in nfl:
+    if row[2] == "New England Patriots":
+        patriots_wins += 1
