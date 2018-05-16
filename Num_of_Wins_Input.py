3fa638e84282ab9cbbb2677f9a36feb65eee303d
Green95GT <39316361+Green95GT@users.noreply.github.com>
2018-05-16 14:44:04 -0400

    Update Num_of_Wins_Input.py

--- Num_of_Wins_Input.py
+++ Num_of_Wins_Input.py
@@ -1,9 +1,3 @@
-import csv
-
-f = open("nfl.csv", 'r')
-nfl = list(csv.reader(f))
-
-# Define your function here.
 def nfl_wins(team):
     count = 0
     for row in nfl:
@@ -11,5 +5,14 @@ def nfl_wins(team):
             count = count + 1
     return count
 
-cowboys_wins = nfl_wins("Dallas Cowboys")
-falcons_wins = nfl_wins("Atlanta Falcons")
+import csv
+
+def main():
+    f = open("nfl.csv", 'r')
+    nfl = list(csv.reader(f))
+
+    cowboys_wins = nfl_wins("Dallas Cowboys")
+    falcons_wins = nfl_wins("Atlanta Falcons")
+
+if __name__ == '__main__':
+    main()
