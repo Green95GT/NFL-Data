8cc26919a316f3298e8ccd441070fd92141df5aa
Green95GT <39316361+Green95GT@users.noreply.github.com>
2018-05-16 14:41:32 -0400

    Update Num_of_Wins_Fixed.py

--- Num_of_Wins_Fixed.py
+++ Num_of_Wins_Fixed.py
@@ -1,10 +1,15 @@
 import csv
-f = open("nfl.csv", "r")
-nfl = list(csv.reader(f))
-patriots_wins = 0
 
-# Loop through the list of rows, each time "New England Patriots" is found
-# in the third column (index value of 2), increase `patriots_wins` by 1
-for row in nfl:
-    if row[2] == "New England Patriots":
-        patriots_wins += 1
+def main():
+    f = open("nfl.csv", "r")
+    nfl = list(csv.reader(f))
+    patriots_wins = 0
+
+    # Loop through the list of rows, each time "New England Patriots" is found
+    # in the third column (index value of 2), increase `patriots_wins` by 1
+    for row in nfl:
+        if row[2] == "New England Patriots":
+            patriots_wins += 1
+
+if __name__ == '__main__':
+    main()            
